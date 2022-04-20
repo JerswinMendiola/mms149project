8657919fd7524908d528b7b25c76d7936e0c9b47
MilkAndBanana <mjerswinmc@gmail.com>
2022-04-20 22:50:21 +0800

    Removed project files to test Git GUI because I'm big doodoo brain.

--- Project Files/Main/Confirm/No.gd
+++
@@ -1,4 +0,0 @@
-extends Button
-
-func _on_No_pressed() -> void:
-	get_parent().get_parent().get_parent().visible = false
--- Project Files/Start.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-func _ready() -> void:
-	get_node("Login").visible = true
-	get_node("Main").visible = false
-	get_node("Data").visible = false
